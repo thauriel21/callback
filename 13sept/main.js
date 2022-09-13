adb105f0a411933cde8af5a67b2c34fdfaa23a4e
Alynson Roldan <alynsonrolfu@gmai.com>
2022-09-13 14:36:41 -0300

    creamos dos archivos index con materia de javascript

---
+++ 13sept/main.js
@@ -0,0 +1,26 @@
+let name = 'alynson'
+let height = 1.63
+let surname = 'roldan'
+let footwear = 37
+const chicken = 'yellow'
+const age = 21
+
+
+//tipos de datos
+let flag = true // boolean, dicotomica
+let number = 1 // Number, numeros enteros o decimales
+let text =' El texto va entre comillas' // string, texto
+
+//arrays, cada elemento tiene un indice y parte desde 0
+let arr =[1, 3, 4, 8]
+let arr2 =['texto', [1, 2, 3], 4, true]
+
+arr[2]//acceder al valor 4 
+arr[2] = 5 //asignando un valor nuevo
+
+//variableName[index] -> acceder al valor de un arreglo
+
+
+let nothing = null // null representa un valor "vacio"o "nulo"
+let nodef = undefined
+let width
